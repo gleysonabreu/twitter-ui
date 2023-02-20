1d59adbf317a4502349be400fecfa59004bbe115
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-02-20 10:53:30 -0300

    fix image and text

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -5,15 +5,13 @@ export function Tweet() {
   return (
     <div className="flex w-full flex-col justify-center items-start py-6 px-5 border-b border-zinc-200 dark:border-zinc-800">
       <div className="flex gap-3">
-        <div className="w-14 h-14">
-          <Image
-            src={avatarPNG.src}
-            alt="Avatar user"
-            height={56}
-            width={56}
-            className="rounded-full"
-          />
-        </div>
+        <Image
+          src={avatarPNG.src}
+          alt="Avatar user"
+          height={56}
+          width={56}
+          className="rounded-full w-14 h-14"
+        />
         <div className="flex flex-col items-start gap-2">
           <div className="flex gap-1 items-center">
             <h1 className="font-bold text-brand-text-black-900 dark:text-gray-100 text-base leading-tight">
@@ -23,16 +21,14 @@ export function Tweet() {
               @johndoe
             </span>
           </div>
-          <p className="text-base  leading-tight text-zinc-900 dark:text-gray-100">
+          <p className="text-base w-4/5 lg:w-full leading-tight text-zinc-900 dark:text-gray-100">
             Acabei de migrar um projeto React GIGANTE de create-react-app para
-            Vite e os resultados foram:
-            <br />✅ npm start: De 32s para 400ms (sim, demorava 30s) <br />✅
-            npm build: De 120s para 22s <br />
-            Além disso, troquei do Yarn para o PNPM e o install das deps mudou
-            de 24s para 8s 🔥
+            Vite e os resultados foram: ✅npm start: De 32s para 400ms (sim,
+            demorava 30s) ✅npm build: De 120s para 22s Além disso, troquei do
+            Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥
           </p>
 
-          <footer className="flex pt-3 gap-12">
+          <footer className="flex pt-3 gap-3 lg:gap-12">
             <button className="flex gap-2 text-zinc-500 dark:text-gray-600 hover:text-brand-primary transition-colors">
               <ChatCircle size={24} />
               <span>32</span>
